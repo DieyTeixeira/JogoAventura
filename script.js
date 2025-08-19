218454b263d611e7f07b74bf51c112bdfcc0095a
DieyTeixeira <dieinison.teixeira@gmail.com>
2025-08-19 15:10:58 -0300

    JogoAventura
    +

--- script.js
+++ script.js
@@ -388,19 +388,19 @@ function showMessage(chestNumber) {
                             openClass = 'openedtrem';
                             newSize = interactedChest.dataset.openedItemSize;
                             deslocX = tileSize * 0.15;
-                            deslocY = tileSize * 0.72;
+                            deslocY = tileSize * 0.7;
                             break;
                         case 6:
                             openClass = 'openedflechas';
                             newSize = interactedChest.dataset.openedItemSize;
                             deslocX = tileSize * 0.15;
-                            deslocY = tileSize * 0.72;
+                            deslocY = tileSize * 0.7;
                             break;
                         case 8:
                             openClass = 'openedcasa';
                             newSize = interactedChest.dataset.openedItemSize;
                             deslocX = tileSize * 0.15;
-                            deslocY = tileSize * 0.72;
+                            deslocY = tileSize * 0.7;
                             break;
                     }
 
