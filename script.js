fafe50edc935a1dad47ea065b7c6d56d25998861
DieyTeixeira <dieinison.teixeira@gmail.com>
2025-08-19 16:04:59 -0300

    JogoAventura
    +

--- script.js
+++ script.js
@@ -367,8 +367,9 @@ function showMessage(chestNumber) {
                 modalImage.style.display = 'none';
                 modalText.innerHTML = `
                     <div class="fade-in">
-                        <img src="${info.img}" alt="${info.nome}" style="width: 80%;">
-                        <h2 style="font-size: 1.6em;">${info.nome}</h2>
+                        <img src="${info.img}" alt="${info.nome}" 
+                            style="width: 80%; display:block; margin:0 auto 10px;">
+                        <h2 style="font-size: 1.8em;">${info.nome}</h2>
                         <p style="font-size: 0.9em;">${info.desc}</p>
                         <p style="margin-top: 1em; font-size: 1.1em;">${info.text}</p>
                     </div>
